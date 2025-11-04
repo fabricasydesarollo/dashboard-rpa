23d8c962020e15ab53cd4a1c5dbbb66536e29f3b
MAYKOL STIVEEN PLAZA CASTRILLON <maykol.plazac@zentria.com.co>
2025-11-04 09:29:58 -0500

    se agrego el campo duracion a registros_general y trazabilidad_hc

--- controllers/socket.controller.js
+++ controllers/socket.controller.js
@@ -182,7 +182,8 @@ export const SocketController = {
             await trazabilidad.update({
               estado_envio: data.estado_envio || 'pendiente',
               motivo_fallo: data.motivo_fallo || null,
-              fecha_envio: data.fecha_envio || new Date()
+              fecha_envio: data.fecha_envio || new Date(),
+              duracion: data.duracion || null
             }, { transaction: t });
           }else{ omitir = true}
         } else {
@@ -192,7 +193,8 @@ export const SocketController = {
             maquina_id: data.maquina_id || 1,
             estado_envio: data.estado_envio || 'pendiente',
             motivo_fallo: data.motivo_fallo || null,
-            fecha_envio: data.fecha_envio || null
+            fecha_envio: data.fecha_envio || null,
+            duracion: data.duracion || null
           }, { transaction: t });
         }
 
