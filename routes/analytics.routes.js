c05cc930fd0df0be9952d7fe7e862441e4d722fc
MAYKOL STIVEEN PLAZA CASTRILLON <maykol.plazac@zentria.com.co>
2025-12-02 09:37:33 -0500

    se protegieron las rutas de analitycs

--- routes/analytics.routes.js
+++ routes/analytics.routes.js
@@ -5,12 +5,12 @@ import { AnalyticsController } from '../controllers/analytics.controller.js';
 const router = Router();
 
 // actualizar datos del usuario
-router.get('/kpis', AnalyticsController.getKpis);
-router.get('/registros-bots', AnalyticsController.getRegistrosPorBotHoy);
-router.get('/procesados-por-maquina', AnalyticsController.getProcesadosPorMaquina);
-router.get('/envios-historias', AnalyticsController.getEnviosHistoriasClinicas);
-router.get('/tiempos-ejecucion', AnalyticsController.getTiemposEjecucion);
-router.get('/distribucion-estados', AnalyticsController.getDistribucionEstados);
-router.get('/solicitudes-inactivacion', AnalyticsController.getSolicitudesInactivacion);
+router.get('/kpis', authenticateToken, AnalyticsController.getKpis);
+router.get('/registros-bots', authenticateToken, AnalyticsController.getRegistrosPorBotHoy);
+router.get('/procesados-por-maquina', authenticateToken, AnalyticsController.getProcesadosPorMaquina);
+router.get('/envios-historias', authenticateToken, AnalyticsController.getEnviosHistoriasClinicas);
+router.get('/tiempos-ejecucion', authenticateToken, AnalyticsController.getTiemposEjecucion);
+router.get('/distribucion-estados', authenticateToken, AnalyticsController.getDistribucionEstados);
+router.get('/solicitudes-inactivacion', authenticateToken, AnalyticsController.getSolicitudesInactivacion);
 
 export default router;
