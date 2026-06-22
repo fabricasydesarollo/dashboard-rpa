2caccf8db7910d36244603c2f667da3c53227577
Luis Franco <lafrancol05@gmail.com>
2026-06-22 08:41:04 -0500

    ajuste de consulta

--- models/go_armenia.js
+++ models/go_armenia.js
@@ -3,26 +3,25 @@ import { executeQuery } from "../db/go_database.js";
 export async function getPacientesGo (fecha_inicio, fecha_fin) {
     try {
         const query = `
-                DECLARE @Empresa int 
-                DECLARE @Sede int
-                DECLARE @fecini datetime
-                DECLARE @fecfin datetime
+            DECLARE @Empresa int 
+            DECLARE @fecEgrInicio datetime
+            DECLARE @fecEgrFin datetime
+            --DECLARE @Contrato int 
 
-                SET @Empresa = 740006
-                SET @Sede = 45
-                SET @fecini = '${fecha_inicio} 00:00:01'
-                SET @fecfin = '${fecha_fin} 23:59:59'
+            SET @Empresa = 740006
+            SET @fecEgrInicio = '${fecha_inicio} 00:00:01'
+            SET @fecEgrFin = '${fecha_fin} 23:59:59'
+            --SET @Contrato = 1039
 
-                SELECT top 10 C.NAME AS Oficina, UCTD.code AS Tipo_documento, U.documentNumber AS Documento,
-                    ltrim(rtrim(U.givenname)) + ' ' + ltrim(rtrim(U.familyname)) as Paciente, E.dateStart as Fecha_ingreso, E.identifier as Num_ingreso
-                FROM [dbo].[encounters] E WITH (NOLOCK) 
-                    INNER JOIN [dbo].[companyOffices] C WITH (NOLOCK) ON C.idUserCompany = E.idUserCompany AND C.IDOFFICE = E.IDOFFICE
-                    INNER JOIN [dbo].[companies] D WITH (NOLOCK) ON D.idUserCompany = C.idUserCompany
-                    INNER JOIN [dbo].[users] F WITH (NOLOCK) ON E.idUserCompany = F.idUser
-                    INNER JOIN [dbo].[users] U WITH (NOLOCK) ON E.idUserPATIENT = U.idUser
-                    INNER JOIN [dbo].[userConfTypeDocuments] UCTD WITH (NOLOCK) ON U.IdDocumentType = UCTD.IdTypeDocument
-                where E.idUserCompany = @Empresa and E.idOffice = @Sede AND E.dateStart between @fecini and @fecfin
-                ORDER BY 1,2,3
+            SELECT BSAE.EncounterClass AS TipoAtencion, BSAE.documentNumber AS Identificacion, BSAE.Patient AS Paciente, 
+                BSAE.dateRegister AS FechaIngreso, E.dateDischarge AS FechaEgreso, 
+                BSAE.dateDischarge AS FechaEstadoCuenta, DATEDIFF(day, BSAE.dateRegister, E.dateDischarge) AS Estancia,
+                BSAE.EncounterNumber AS NumAtencion, BSAH.number AS NumEstadoCuenta, BSAH.status AS EstadoCuenta
+            FROM BillStateOfAccountEncounters BSAE
+                INNER JOIN encounters E WITH (NOLOCK) ON BSAE.idencounter = E.idencounter
+                INNER JOIN BillStateOfAccountHeader BSAH WITH (NOLOCK) ON BSAE.idStateOfAccountHeader = BSAH.idStateOfAccountHeader
+            WHERE E.idUserCompany = @Empresa AND BSAH.status = 'FC' AND BSAE.documentNumber != ''
+                AND BSAE.dateDischarge BETWEEN @fecEgrInicio AND @fecEgrFin;
             `
 
         const resultado = await executeQuery(query);
